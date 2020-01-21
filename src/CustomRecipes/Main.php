d8112f13dec828206f7326b585ae1b91c3752f83
Adam Le <adamsuper169@gmail.com>
2020-01-21 01:29:28 -0800

    Update Main.php

--- src/CustomRecipes/Main.php
+++ src/CustomRecipes/Main.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace CustomRecipes;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use RPGCraft\Recipes\Recipes;
+class Main extends PluginBase implements Listener{
+	
+	private $craft;
+	
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->craft = new Recipes($this);
+	}
+	
+	public function onJoin(PlayerJoinEvent $event){
+		if($this->craft instanceof Recipes){
+			$event->getPlayer()->dataPacket($this->craft->getCraftingDataPacket());
+		}
+	}
+}

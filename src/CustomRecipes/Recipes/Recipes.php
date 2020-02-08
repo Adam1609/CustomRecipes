b09d85a9290d982a2b2b3e4b8c1737320d2dd253
Adam Le <adamsuper169@gmail.com>
2020-02-08 05:00:00 -0800

    Create Recipes.php

---
+++ src/CustomRecipes/Recipes/Recipes.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace RPGCraft\Recipes;
+
+use pocketmine\inventory\ShapedRecipe;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance; 
+use pocketmine\item\Item;
+use pocketmine\network\mcpe\protocol\BatchPacket;
+use pocketmine\network\mcpe\protocol\CraftingDataPacket;
+
+use RPGCraft\Main;
+
+class Recipes{
+	
+	private $plugin;
+	
+	private $craftingDataCache;
+	
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+		$this->init();
+	}
+	
+	public function init(): void{
+		$recipes = $this->getRecipes();
+		
+		$pk = new CraftingDataPacket();
+		$pk->cleanRecipes = true;
+		
+		foreach($recipes as $recipe){
+			$ing = array_map(function(array $data): Item{return Item::jsonDeserialize($data); }, $recipe['input']);
+			$res = Item::get($recipe['result']['id'], $recipe['result']['meta'], $recipe['result']['count']);
+			
+			foreach($recipe['result']['enchantments'] as $key => $level){
+				$enchantment = Enchantment::getEnchantmentByName($key);
+				//$enchantment = CustomEnchantManager::getEnchantmentByName($key);
+				$res->addEnchantment(new EnchantmentInstance($enchantment, $level));
+			}
+			$pk->addShapedRecipe(new ShapedRecipe($recipe['shape'], $ing, [$res]));
+			$this->plugin->getServer()->getCraftingManager()->registerShapedRecipe(new ShapedRecipe($recipe['shape'], $ing, [$res]));
+		}
+		
+		foreach($this->plugin->getServer()->getCraftingManager()->getShapedRecipes() as $list){
+			foreach($list as $recipe){
+				
+				if($recipe instanceof ShapedRecipe){
+					foreach($recipe->getResults() as $res){
+						foreach($recipes as $rec){
+							if($res->getId() == $rec['replace']['id']){
+								break 3;
+							}
+						}
+					}
+				}
+				
+				$pk->addShapedRecipe($recipe);
+			}
+		}
+		
+		$batch = new BatchPacket();
+		$batch->addPacket($pk);
+		$batch->setCompressionLevel($this->plugin->getServer()->getInstance()->networkCompressionLevel);
+		$batch->encode();
+		
+		$this->craftingDataCache = $batch;
+	}
+	
+	public function getRecipes(): array{
+		return json_decode(file_get_contents($this->plugin->getServer()->getDataPath().'/plugins/CustomCraft-master/src/RPGCraft/Recipes/Recipes.json'), true);
+	}
+	
+	public function getCraftingDataPacket(): BatchPacket{
+		return $this->craftingDataCache;
+	}
+}
